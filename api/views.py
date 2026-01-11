ed1daff241b8bd4e555c3bfbe7a51373ca410055
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-11 14:43:32 +0600

    Added DRF response
    Now we have used DRF response. This will give us a simple Renderer (GUI) in the browser to operate API endpoints.

--- api/views.py
+++ api/views.py
@@ -1,12 +1,17 @@
 from django.http import JsonResponse
+
 from api.serializers import ProductSerializer
 from api.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET'])
 def product_list(request):
     all_products = Product.objects.all()
     serializer = ProductSerializer(all_products, many=True)      # If we pass more than one product than many=False
-    return JsonResponse({
-        'data': serializer.data
-    })
+    # return Response({'data': serializer.data})
+    # or,
+    return Response(serializer.data)
