82ddceff956ca3ad11c5466482885a44786b2de2
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-12 09:51:22 +0600

    Write serializer without models.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Max
 
-from api.serializers import ProductSerializer, OrderSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 
 from rest_framework.response import Response
@@ -28,4 +29,15 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'product': products,
+        'count': len(products),
+        'max_price': products.aaggregate(max_price=Max('price'))['max_price']
+    })
     return Response(serializer.data)
