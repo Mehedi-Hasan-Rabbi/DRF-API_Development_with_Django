4b00d2e68753381eedcdefe2a787f6ebb95063c1
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-12 08:32:34 +0600

    Using Serializer Method field.

--- api/serializers.py
+++ api/serializers.py
@@ -34,6 +34,17 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
 
+    # Calculated total price of the order
+    total_price = serializers.SerializerMethodField()
+    # This is working by 'get_total_price' function. 
+    # It is default to use get_variable_name. 
+    # Check DRF SerializerMethodField for more info.
+    # If you want to use custom name then you can pass 'method_name' argument in SerializerMethodField.
+
+    def get_total_price(self, obj):     # Here object is refering to Order
+        order_items = obj.items.all()   # We have defined related_name='items' in OrderItem model.
+        return sum(order_item.item_subtotal for order_item in order_items)
+
     class Meta:
         model = Order
         fields = (
@@ -41,5 +52,6 @@ class OrderSerializer(serializers.ModelSerializer):
             'user',
             'created_at',
             'status',
-            'items'
+            'items',
+            'total_price',
         )
