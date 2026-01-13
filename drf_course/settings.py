a9615ad81ffc625b7dab76a8a25ab154b5d3f773
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-13 14:52:34 +0600

    Added JWT and make is as First priority to DEFAULT_AUTHENTICATION_CLASSES

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -128,3 +128,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 AUTH_USER_MODEL = 'api.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
