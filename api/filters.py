2dbc0aa555700d38a733bd0dcef7dfb58a3f8261
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-14 11:40:14 +0600

    Case-Sensitive handled, greater then, less then, range

--- api/filters.py
+++ api/filters.py
@@ -4,4 +4,7 @@ from api.models import Product
 class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
-        fields = ('name', 'price')
+        fields = {
+            'name': ['exact', 'iexact', 'contains', 'icontains'], 
+            'price': ['exact', 'lt', 'gt', 'range'],
+        }
