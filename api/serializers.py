f718e2f76c66cc304fcd6a70998191afaa05d7d0
Mehedi Hasan Rabbi <57454189+Mehedi-Hasan-Rabbi@users.noreply.github.com>
2026-01-17 14:54:27 +0600

    Wrapping update & create in database transactions

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,7 @@
+from django.db import transaction
+
 from rest_framework import serializers
+
 from .models import Product, Order, OrderItem, User
 
 
@@ -56,24 +59,28 @@ class OrderCreateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         orderitem_data = validated_data.pop('items')
-        instance = super().update(instance, validated_data)
-        
-        if orderitem_data is not None:
-            # Clear existing items (optional, depends on requirements)
-            instance.items.all().delete()
 
-            # Recreate items with the updated data
-            for item in orderitem_data:
-                OrderItem.objects.create(order=instance, **item)
+        with transaction.atomic():
+            instance = super().update(instance, validated_data)
+            
+            if orderitem_data is not None:
+                # Clear existing items (optional, depends on requirements)
+                instance.items.all().delete()
+
+                # Recreate items with the updated data
+                for item in orderitem_data:
+                    OrderItem.objects.create(order=instance, **item)
         
         return instance
 
     def create(self, validated_data):
         orderitem_data = validated_data.pop('items')
-        order = Order.objects.create(**validated_data)
 
-        for item in orderitem_data:
-            OrderItem.objects.create(order=order, **item)
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
+
+            for item in orderitem_data:
+                OrderItem.objects.create(order=order, **item)
 
         return order
 
